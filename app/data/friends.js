b2eef9174eb49b1077d234332cb9086e75ede7e7
Dave Nolan <dpn.nolan@gmail.com>
2019-11-22 09:50:41 -0500

    package json

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+
+var characters = [
+    {
+      routeName: "tobias",
+      name: "Tobias Funke",
+      photo:"../assets/images/tobias.png",
+      quote: "You just blue yourself",
+      scores:[1,1,1,1,1,1,1,1,1,1,]
+    },
+    {
+
+    }
+  ];
